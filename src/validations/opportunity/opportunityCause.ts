cd92489e95a06cf746f553d19b7a27c8023f36d1
paddyRVS <Patrick.Kenneally@royalvoluntaryservice.org.uk>
2025-03-10 13:24:58 +0000

    refactor: Replace Prisma client usage with direct cause validation in opportunity validations

--- src/validations/opportunity/opportunityCause.ts
+++ src/validations/opportunity/opportunityCause.ts
@@ -1,54 +1,27 @@
-import { prisma } from "../../services/prisma";
+import { Cause } from "@prisma/client";
 import type { CsvValidatorResponse } from "../../types/csvValidation";
 import { getCsvValidationResponse } from "../../utils/csvValidation";
 import { toSentenceCase, toHumanString } from "../../utils/string";
 
 
-
-let lastCheckedAt: Date | null = null;
-let lastCauses: string[] = [];
-const defaultCause = () => lastCauses[0] ?? "None set";
-
-const syncLatestCauses = async () => {
-    // if longer than 5 minutes
-    if (lastCauses.length > 0) {
-        if (lastCheckedAt && (new Date().getTime() - lastCheckedAt.getTime()) < 300000) {
-            return;
-        }
-    }
-
-
-    const causes = await prisma.cause.findMany({
-        select: {
-            causeName: true
-        }
-    });
-
-    lastCheckedAt = new Date();
-    lastCauses = causes.map(cause => cause.causeName);
-}
-
-
-
-export const validateOpportunityCause = async (cause: string): Promise<boolean> => {
-    await syncLatestCauses();
-    return lastCauses.includes(cause);
+export const validateOpportunityCause = (cause: string, causes: Pick<Cause, 'causeName'>[]): boolean => {
+    return causes.map(cause => cause.causeName).includes(cause);
 };
 
-export const validateOpportunityCauseForCSV = async (type?: string | number | null): Promise<null | CsvValidatorResponse> => {
-    await syncLatestCauses();
+export const validateOpportunityCauseForCSV = (type: string | number | null | undefined, causes: Pick<Cause, 'causeName'>[]): null | CsvValidatorResponse => {
+
 
     function toReturn(): CsvValidatorResponse {
         return getCsvValidationResponse(
             "healing",
-            "Opportunity Cause should be one of: " + lastCauses.join(', '),
-            toSentenceCase(toHumanString(defaultCause()))
+            "Opportunity Cause should be one of: " + causes.map(cause => cause.causeName).join(', '),
+            toSentenceCase(toHumanString(causes[0]?.causeName ?? "None set"))
         )
     }
     if (!type || typeof type === "number") {
         return toReturn()
     }
-    const isValidCase = await validateOpportunityCause(type)
+    const isValidCase = validateOpportunityCause(type, causes)
     if (!isValidCase) {
         return toReturn()
     }
