b5773d80199de3007725a7ca073ed24f5712ebfb
paddyRVS <Patrick.Kenneally@royalvoluntaryservice.org.uk>
2025-03-17 16:20:28 +0000

    chore: consolidate validation exports into a single entry point

---
+++ src/validations/index.ts
@@ -0,0 +1,6 @@
+export * from './opportunity/opportunityCause';
+export * from './opportunity/opportunityCommitment';
+export * from './opportunity/opportunityStatus';
+export * from './opportunity/opportunityType';
+export * from './opportunity/validateOpportunityCsvRow';
+export * from './primitive/boolean';
